fc72f742427233f28a27d924139792f6c7632b32
vascYT <nicovanaschaufler@gmail.com>
2022-01-11 18:30:11 +0100

    🖌️ Restyle, 🩹 Bug fixes, 👍 Improvements

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const app = express();
 const fs = require("fs");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
-const morgan = require("morgan");
 const shortid = require("shortid");
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -14,25 +14,25 @@ app.use(fileUpload({ createParentPath: true }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/", (req, res) => {
+app.post("/upload", (req, res) => {
   try {
     const file = req.files.file;
     const id = shortid.generate();
     const extension = file.name.split(".")[file.name.split(".").length - 1];
-    const allowedExtensions = ["png", "jpeg", "jpg", "gif", "pnga", "svg"];
+    const allowedExtensions = ["png", "jpeg", "jpg", "gif", "pnga"];
 
     if (!allowedExtensions.includes(extension)) {
-      res.redirect("/");
+      res.render("message", { message: "Unsupported file type" });
       return;
     }
 
-    file.mv(`./public/source/${id}.${extension}`);
+    const filePath = path.join(__dirname, "uploads", `${id}.${extension}`);
+    file.mv(filePath);
 
     res.redirect(`/${id}.${extension}`);
   } catch (err) {
@@ -44,21 +44,51 @@ app.post("/", (req, res) => {
 app.get("/:filename", (req, res) => {
   try {
     const fileName = req.params.filename;
+    const filePath = path.join(__dirname, "uploads", fileName);
 
-    const fileStats = fs.statSync(`${__dirname}/public/source/${fileName}`);
+    const fileStats = fs.statSync(filePath);
     const fileDate = `${fileStats.birthtime.toDateString()}, ${fileStats.birthtime.getHours()}:${fileStats.birthtime.getMinutes()}`;
 
     res.render("fileviewer", {
       fileName: fileName,
-      fileMb: Math.round((fileStats.size / (1024 * 1024)) * 100) / 100,
+      fileSize: humanFileSize(fileStats.size, true, 1),
       fileDate: fileDate,
     });
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    res.render("message", { message: "File not found" });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Up and listening: http://localhost:${PORT}`);
+app.get("/source/:filename", (req, res) => {
+  res.sendFile(path.join(__dirname + "/uploads/" + req.params.filename));
 });
+
+app.listen(PORT, () => {
+  console.log(`🆙 at http://localhost:${PORT}`);
+});
+
+function humanFileSize(bytes, si = false, dp = 1) {
+  // Thanks to mpen (https://stackoverflow.com/a/14919494/12282885)
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+
+  const units = si
+    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  );
+
+  return bytes.toFixed(dp) + " " + units[u];
+}
